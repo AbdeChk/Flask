905b58b5c71e3eec955f6d4fd496caee89e9165d
AbdeChk <abdechkoundi@gmail.com>
2023-12-18 16:42:36 +0100

    updat-V1

--- application/routes.py
+++ application/routes.py
@@ -3,7 +3,7 @@ from application.models import Income
 
 from flask import render_template, flash, redirect, url_for, get_flashed_messages
 from application.forms import UserInputForm
-
+import json
 
 # dispay the content of db in index 
 @app.route('/')
@@ -29,7 +29,7 @@ def add_expenses():
     return render_template('add.html',title = 'add', form = form )
 
 
-# delete from 
+# delete from
 @app.route('/delete/<int:entry_id>')
 def delete(entry_id):
     try:
@@ -44,3 +44,30 @@ def delete(entry_id):
         db.session.close()
     
     return redirect(url_for('index'))
+
+# dashboard_rout 
+@app.route('/dash')
+def dash():
+      
+    # get the total of income and expens   
+    income_vs_expenses = db.session.query(db.func.sum(Income.amount),Income.type).group_by(Income.type).order_by(Income.type).all()
+
+    # get_dates 
+    dates = db.session.query(db.func.sum(Income.amount), Income.date).group_by(Income.date).order_by(Income.date).all()
+       
+    income_expense_data = []
+    for total_amount, _ in income_vs_expenses:
+        income_expense_data.append(total_amount)
+        
+    time_data = []
+    time_labels = []
+    for amount, date in dates:
+        time_data.append(amount)
+        time_labels.append(date.strftime('%m-%d-%y')) 
+
+
+    return render_template('dash.html', 
+                           income_vs_expenses = json.dumps(income_expense_data),
+                           time_data = json.dumps(time_data),
+                           time_labels = json.dumps(time_labels)
+                           )
